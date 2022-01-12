d66a13f7c2944a01989a08a7673c43f818e2a1bb
Lucas Mesquita <lucas.cas.mesquita@gmail.com>
2022-01-12 19:23:47 -0300

    Ajustes lint

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
 function login() {
-    let nomeUsuario = document.getElementById('nomeUsuario').value;
-    let senha = document.getElementById('senha').value;
+  const nomeUsuario = document.getElementById('nomeUsuario').value;
+  const senha = document.getElementById('senha').value;
 
-    if (nomeUsuario === "tryber@teste.com" && senha === "123456"){
-       alert("Olá, Tryber!")
-    }
-    else{
-        alert("Email ou senha inválidos.")
-    }
+  if (nomeUsuario === 'tryber@teste.com' && senha === '123456') {
+    alert('Olá, Tryber!');
+  } else {
+    alert('Email ou senha inválidos.');
+  }
 }
 
-function habilitaBotao () {
-  let recebe = document.getElementById('agreement');
-  let recebeBotao = document.getElementById('submit-btn');
+function habilitaBotao() {
+  const recebe = document.getElementById('agreement');
+  const recebeBotao = document.getElementById('submit-btn');
   if (recebe.checked === true) {
     recebeBotao.disabled = false;
   } else {
@@ -20,84 +19,84 @@ function habilitaBotao () {
   }
 }
 
-function contaCaracteres (textArea, idContador) {
-  let maxCar = 500;
-  let recebe = document.getElementById(idContador);
+function contaCaracteres(textArea, idContador) {
+  const maxCar = 500;
+  const recebe = document.getElementById(idContador);
   recebe.innerText = maxCar - (textArea.value.length);
 }
 
-function showName () {
-  let recebe = document.getElementById('show-info');
-  let recebeNome = document.getElementById('input-name');
-  let recebeSobrenome = document.getElementById('input-lastname');
-  let cria = document.createElement('p');
-  cria.innerText = 'Nome: ' + recebeNome.value + ' ' + recebeSobrenome.value;
+function showName() {
+  const recebe = document.getElementById('show-info');
+  const recebeNome = document.getElementById('input-name');
+  const recebeSobrenome = document.getElementById('input-lastname');
+  const cria = document.createElement('p');
+  cria.innerText = `Nome:  ${recebeNome.value} ' ' ${recebeSobrenome.value}`;
   recebe.appendChild(cria);
 }
 
-function showEmail () {
-  let recebe = document.getElementById('show-info');
-  let recebeInput = document.getElementById('input-email');
-  let cria = document.createElement('p');
-  cria.innerText = 'Email: ' + recebeInput.value;
+function showEmail() {
+  const recebe = document.getElementById('show-info');
+  const recebeInput = document.getElementById('input-email');
+  const cria = document.createElement('p');
+  cria.innerText = `Email:  ${recebeInput.value}`;
   recebe.appendChild(cria);
 }
 
-function showHouse () {
-  let recebe = document.getElementById('show-info');
-  let recebeInput = document.getElementById('house');
-  let cria = document.createElement('p');
-  cria.innerText = 'Casa: ' + recebeInput.value;
+function showHouse() {
+  const recebe = document.getElementById('show-info');
+  const recebeInput = document.getElementById('house');
+  const cria = document.createElement('p');
+  cria.innerText = `Casa: ${recebeInput.value}`;
   recebe.appendChild(cria);
 }
 
-function showFamily () {
-  let recebe = document.getElementById('show-info');
-  let recebeInput = document.querySelectorAll('.family');
-  let cria = document.createElement('p');
+function showFamily() {
+  const recebe = document.getElementById('show-info');
+  const recebeInput = document.querySelectorAll('.family');
+  const cria = document.createElement('p');
   cria.innerText = 'Família: ';
-  for (let index = 0; index < recebeInput.length ; index++){
+  for (let index = 0; index < recebeInput.length; index++) {
     if (recebeInput[index].checked === true) {
-      cria.innerText+= recebeInput[index].value;
+      cria.innerText += recebeInput[index].value;
     }
   }
   recebe.appendChild(cria);
 }
 
-function showContent () {
-  let recebe = document.getElementById('show-info');
-  let recebeInput = document.querySelectorAll('.subject');
-  let cria = document.createElement('p');
+function showContent() {
+  const recebe = document.getElementById('show-info');
+  const recebeInput = document.querySelectorAll('.subject');
+  const cria = document.createElement('p');
   cria.innerText = 'Matérias:';
   for (let index = 0; index < recebeInput.length; index++) {
     if (recebeInput[index].checked === true)
-    cria.innerText+= ' ' + recebeInput[index].value;
+    cria.innerText += ' ' + recebeInput[index].value;
   }
   recebe.appendChild(cria);
 }
 
-function showRate () {
-  let recebe = document.getElementById('show-info');
-  let recebeInput = document.querySelectorAll('.rate');
-  let cria = document.createElement('p');
+function showRate() {
+  const recebe = document.getElementById('show-info');
+  const recebeInput = document.querySelectorAll('.rate');
+  const cria = document.createElement('p');
   cria.innerText = 'Avaliação: ';
-  for ( let index = 0 ; index < recebeInput.length ; index++) {
+  for (let index = 0 ; index < recebeInput.length; index++) {
     if (recebeInput[index].checked === true) {
-      cria.innerText+= recebeInput[index].value;
+      cria.innerText += recebeInput[index].value;
     }
   }
   recebe.appendChild(cria);
 }
 
-function showObserves () {
-  let recebe = document.getElementById('show-info');
-  let recebeInput = document.getElementById('textarea');
-  let cria = document.createElement('p');
-  cria.innerText = 'Observações: ' + recebeInput.value;
+function showObserves() {
+  const recebe = document.getElementById('show-info');
+  const recebeInput = document.getElementById('textarea');
+  const cria = document.createElement('p');
+  cria.innerText = `Observações: ${recebeInput.value}`;
   recebe.appendChild(cria);
 }
 
-function criaRetorno () {
+function criaRetorno() {
   showName();
   showEmail();
   showHouse();
